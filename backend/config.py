e66b2a7e1cfa9a40bb7459026669040db5f33b78
kshitijagarwal31 <kshitijagarwal999@gmail.com>
2026-06-23 11:59:01 +0530

    Add JWT token configuration

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
+from passlib.context import CryptContext 
 import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60*24*7
+
+pwd_context = CryptContext(schemes=["argon2"])
