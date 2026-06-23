54fb5a9af12d2f812a536879bf0750b74e391682
kshitijagarwal31 <kshitijagarwal999@gmail.com>
2026-06-23 11:59:31 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from routes import router
 from database import engine, Base
+import models
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router)
-
 
